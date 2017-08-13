e00b2ad3b9bdc65d37f0b9c791d999cf53c2f7c6
Polina Rylova <mail@polinarya.ru>
2017-08-13 19:54:34 +0500

    Перерисовала облако. Баг заливки и насыщенности не исправлен

--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,26 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  ctx.beginPath(); // Рисуем фигуру тени от облака
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)'; // Цвет контура тени облака
+  ctx.moveTo(190,20);
+  ctx.quadraticCurveTo(55,30,105,125);
+  ctx.quadraticCurveTo(20,350,325,280);
+  ctx.quadraticCurveTo(515,330,460,200);
+  ctx.quadraticCurveTo(690, 10,190,20);
+  ctx.stroke();
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
-  ctx.fillRect(110, 20, 420, 270); // Фигура тени
-  ctx.strokeStyle = 'rgb(135, 206, 235)'; // Цвет контура облака
-  ctx.strokeRect(100, 10, 420, 270); // Фигура контура облака
+  ctx.fill();
+  ctx.beginPath();
+  ctx.strokeStyle = 'rgb(0, 0, 139)'; // Цвет контура облака
+  ctx.moveTo(180,10);
+  ctx.quadraticCurveTo(45,20,95,115);
+  ctx.quadraticCurveTo(10,340,315,270);
+  ctx.quadraticCurveTo(505,320,450,190);
+  ctx.quadraticCurveTo(680, 0,180,10);
+  ctx.stroke();
   ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'; // Цвет фигуры облака
-  ctx.fillRect(100, 10, 420, 270); // Фигура облака
+  ctx.fill();
   ctx.fillStyle = '#dc143c'; // Цвет заголовка
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура! Вы победили!', 120, 40);
@@ -27,17 +41,18 @@ window.renderStatistics = function (ctx, names, times) {
   var indent = 50;
   var initialX = 120;
   var initialY = 100;
+  // var colorSaturation = [];
   ctx.textBaseline = 'top';
   for (var j = 0; j < times.length; j++) {
     if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, Math.random())';
+      // colorSaturation[j] = Math.random().toFixed(1);
+      ctx.fillStyle = 'rgba(0, 0, 255, Math.random().toFixed(0))';
     }
     ctx.fillRect(initialX + (indent + barWidth) * j, initialY + (histogramHeight - times[j] * step), barWidth, times[j] * step);
     ctx.fillText(names[j], initialX + (indent + barWidth) * j, initialY + histogramHeight ); // Подпись с именем игрока
     ctx.fillText(times[j].toFixed(0), initialX + (indent + barWidth) * j, initialY + (histogramHeight - times[j] * step) - 20); // Подпись с результатом игрока
   }
 };
-// Перерисовать прямоугольник в облако
-// Починить случайно задаваемую насыщенность колонок (пока не работает и баг заливки - проверить дебаггером условие заливки (синие заливаются красным)
+// Починить случайно задаваемую насыщенность колонок (не работает насыщенность и заливает красным не только игрока "Вы")
