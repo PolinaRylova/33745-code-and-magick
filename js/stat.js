9917609860f6b3409649e7b8ab17b9724e45525c
Polina Rylova <mail@polinarya.ru>
2017-08-14 12:59:32 +0500

    Поправила отступы по styleguide и вынесла в отдельные функции нахождение максимального времени и случайной насыщенности

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,35 @@
 'use strict';
 
+var findMaxTime = function (times) {
+  var maxTime = -1;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > maxTime) {
+      maxTime = time;
+    }
+  }
+};
+var findRandomSaturation = function () {
+  return Math.random().toFixed(1);
+};
 window.renderStatistics = function (ctx, names, times) {
   ctx.beginPath(); // Рисуем фигуру тени от облака
   ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)'; // Цвет контура тени облака
-  ctx.moveTo(190,20);
-  ctx.quadraticCurveTo(55,30,105,125);
-  ctx.quadraticCurveTo(20,350,325,280);
-  ctx.quadraticCurveTo(515,330,460,200);
-  ctx.quadraticCurveTo(690, 10,190,20);
+  ctx.moveTo(190, 20);
+  ctx.quadraticCurveTo(55, 30, 105, 125);
+  ctx.quadraticCurveTo(20, 350, 325, 280);
+  ctx.quadraticCurveTo(515, 330, 460, 200);
+  ctx.quadraticCurveTo(690, 10, 190, 20);
   ctx.stroke();
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
   ctx.fill();
   ctx.beginPath(); // Рисуем фигуру облака
   ctx.strokeStyle = 'rgb(0, 0, 139)'; // Цвет контура облака
-  ctx.moveTo(180,10);
-  ctx.quadraticCurveTo(45,20,95,115);
-  ctx.quadraticCurveTo(10,340,315,270);
-  ctx.quadraticCurveTo(505,320,450,190);
-  ctx.quadraticCurveTo(680, 0,180,10);
+  ctx.moveTo(180, 10);
+  ctx.quadraticCurveTo(45, 20, 95, 115);
+  ctx.quadraticCurveTo(10, 340, 315, 270);
+  ctx.quadraticCurveTo(505, 320, 450, 190);
+  ctx.quadraticCurveTo(680, 0, 180, 10);
   ctx.stroke();
   ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'; // Цвет фигуры облака
   ctx.fill();
@@ -26,17 +38,9 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура! Вы победили!', 120, 40);
   ctx.fillStyle = '#000080'; // Цвет подзаголовка
   ctx.fillText('Список результатов:', 120, 65);
-  // Определяем худшее время прохождения игры
-  var maxTime = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
-    }
-  }
   // Рисуем гистограмму статистики
   var histogramHeight = 150;
-  var step = histogramHeight / (maxTime - 0);
+  var step = histogramHeight / (findMaxTime() - 0);
   var barWidth = 40;
   var indent = 50;
   var initialX = 120;
@@ -46,11 +50,10 @@ window.renderStatistics = function (ctx, names, times) {
     if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      var colorSaturation = Math.random().toFixed(1);
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + colorSaturation + ' )';
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + findRandomSaturation() + ' )';
     }
     ctx.fillRect(initialX + (indent + barWidth) * j, initialY + (histogramHeight - times[j] * step), barWidth, times[j] * step);
-    ctx.fillText(names[j], initialX + (indent + barWidth) * j, initialY + histogramHeight ); // Подпись с именем игрока
+    ctx.fillText(names[j], initialX + (indent + barWidth) * j, initialY + histogramHeight); // Подпись с именем игрока
     ctx.fillText(times[j].toFixed(0), initialX + (indent + barWidth) * j, initialY + (histogramHeight - times[j] * step) - 20); // Подпись с результатом игрока
   }
 };
