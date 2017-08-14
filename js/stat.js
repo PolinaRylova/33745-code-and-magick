d9b92a5f7bac541aae8d19f15b2a7b00dd8dbc51
Polina Rylova <mail@polinarya.ru>
2017-08-14 13:39:04 +0500

    Вынесла в отдельные функции отрисовку облака, тени, текста заголовков и столбиков гистограммы с подписями. Баг отрисовки гистограммы

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,7 @@
 'use strict';
 
-var findMaxTime = function (times) {
-  var maxTime = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
-    }
-  }
-};
-var findRandomSaturation = function () {
-  return Math.random().toFixed(1);
-};
-window.renderStatistics = function (ctx, names, times) {
-  ctx.beginPath(); // Рисуем фигуру тени от облака
-  ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)'; // Цвет контура тени облака
-  ctx.moveTo(190, 20);
-  ctx.quadraticCurveTo(55, 30, 105, 125);
-  ctx.quadraticCurveTo(20, 350, 325, 280);
-  ctx.quadraticCurveTo(515, 330, 460, 200);
-  ctx.quadraticCurveTo(690, 10, 190, 20);
-  ctx.stroke();
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
-  ctx.fill();
-  ctx.beginPath(); // Рисуем фигуру облака
+var drawStatisticCloud = function (ctx) {
+  ctx.beginPath();
   ctx.strokeStyle = 'rgb(0, 0, 139)'; // Цвет контура облака
   ctx.moveTo(180, 10);
   ctx.quadraticCurveTo(45, 20, 95, 115);
@@ -33,14 +11,46 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.stroke();
   ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'; // Цвет фигуры облака
   ctx.fill();
+};
+
+var drawStatisticCloudShadow = function (ctx) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)'; // Цвет контура тени облака
+  ctx.moveTo(190, 20);
+  ctx.quadraticCurveTo(55, 30, 105, 125);
+  ctx.quadraticCurveTo(20, 350, 325, 280);
+  ctx.quadraticCurveTo(515, 330, 460, 200);
+  ctx.quadraticCurveTo(690, 10, 190, 20);
+  ctx.stroke();
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
+  ctx.fill();
+};
+
+var writeStatisticText = function (ctx) {
   ctx.fillStyle = '#dc143c'; // Цвет заголовка
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура! Вы победили!', 120, 40);
   ctx.fillStyle = '#000080'; // Цвет подзаголовка
   ctx.fillText('Список результатов:', 120, 65);
-  // Рисуем гистограмму статистики
+};
+
+var findMaxTime = function (times) {
+  var maxTime = -1;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > maxTime) {
+      maxTime = time;
+    }
+  }
+};
+
+var findRandomSaturation = function () {
+  return Math.random().toFixed(1);
+};
+
+var drawStatisticHistogram = function (ctx, names, times) {
   var histogramHeight = 150;
-  var step = histogramHeight / (findMaxTime() - 0);
+  var step = histogramHeight / (findMaxTime(times) - 0);
   var barWidth = 40;
   var indent = 50;
   var initialX = 120;
@@ -56,4 +66,11 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(names[j], initialX + (indent + barWidth) * j, initialY + histogramHeight); // Подпись с именем игрока
     ctx.fillText(times[j].toFixed(0), initialX + (indent + barWidth) * j, initialY + (histogramHeight - times[j] * step) - 20); // Подпись с результатом игрока
   }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  drawStatisticCloudShadow(ctx);
+  drawStatisticCloud(ctx);
+  writeStatisticText(ctx);
+  drawStatisticHistogram(ctx, names, times);
 };
