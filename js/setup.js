3b85b911b8915ad316063d4dc6101486dc2466b4
Polina Rylova <mail@polinarya.ru>
2017-08-16 17:19:35 +0500

    Визуализировала блок для похожих персонажей и склонировала шаблон в элемент списка

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,18 @@
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+userDialog.classList.remove('hidden'); // Показываю окно настроек
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden'); // Показываю заголовок "Похожие персонажи"
+
+var similarListElement = userDialog.querySelector('.setup-similar-list'); // Нахожу список, в который буду вставдять персонажей
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content; // Нахожу контент шаблона волшебника
+
+// var WIZARDS_NAMES = [];
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  similarListElement.appendChild(wizardElement);
+}
+
