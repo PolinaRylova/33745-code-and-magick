40be3de4ca593109d86dc9e1ae6d82ee3b58deb1
Polina Rylova <miracleserenity@gmail.com>
2017-09-09 12:29:40 +0300

    Добавила фильтрацию магов и устранение дребезга

--- js/util.js
+++ js/util.js
@@ -2,12 +2,6 @@
 (function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.classList.add('error-message');
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
   window.util = {
     isEscEvent: function (e, action) {
       if (e.keyCode === ESC_KEYCODE) {
@@ -21,7 +15,6 @@
     },
     getRandomIndex: function (min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
-    },
-    errorHandler: errorHandler
+    }
   };
 })();
